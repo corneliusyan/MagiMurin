4d05d09545362dce2590f1402a34527bb63f03fa
corneliusyan <onel.raja@gmail.com>
2019-01-17 22:06:00 +0700

    Initial Commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Cornelius Yan Mintareja on 17/01/19.
+//  Copyright © 2019 Cornelius Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
